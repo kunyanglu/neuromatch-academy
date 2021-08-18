95671266a0c727230fc39ecaf0f80f48d7adabbb
kunyanglu <lualicia88@gmail.com>
2021-08-18 12:52:42 -0700

    Implement a variational autoencoder

--- W2/VAE.py
+++ W2/VAE.py
@@ -1,1 +1,23 @@
+import torch
+import random
 
+import numpy as np
+import matplotlib.pylab as plt
+from sklearn.decomposition import PCA
+
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+import torchvision
+from torchvision import datasets, transforms
+
+from pytorch_pretrained_biggan import BigGAN
+from pytorch_pretrained_biggan import one_hot_from_names
+from pytorch_pretrained_biggan import truncated_noise_sample
+
+from tqdm.notebook import tqdm, trange
+
+SEED = 2021
+
+DEVICE = 'cpu'
